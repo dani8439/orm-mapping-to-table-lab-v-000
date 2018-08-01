ef79e9bedf58c4ca6b6a86c1a25c1034eea90931
Dani Schuhman <dani8439@aol.com>
2018-08-01 17:59:52 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,7 +15,14 @@ class Student
     @@all << self
   end
 
-  def self.create_table 
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
+    SQL
   end
 
 end
